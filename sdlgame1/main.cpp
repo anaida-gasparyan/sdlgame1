ab4a3b73ce338cf21a77579268ae6cad48b4dc0a
anaida-gasparyan <anaida.gasparyan@gmail.com>
2018-11-24 18:15:54 +0100

    Initial Commit

---
+++ sdlgame1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sdlgame1
+//
+//  Created by Anaida Gasparyan on 11/24/18.
+//  Copyright © 2018 Anaida Gasparyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
