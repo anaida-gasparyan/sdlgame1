bbfcc484f5b15f5a145372857307fc60d9ecc0ed
anaida-gasparyan <anaida.gasparyan@gmail.com>
2018-11-26 22:15:33 +0100

    Add texture

--- sdlgame1/src/Game.cpp
+++ sdlgame1/src/Game.cpp
@@ -28,13 +28,38 @@ bool Game::init(const char* title, int xpos, int ypos, int width,
         return false; // renderer init fail
     }
     
-    SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+//    SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+    
+    SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+    if (pTempSurface == 0) {
+        std::cout << "surface creation fail\n";
+        return false; // renderer init fail
+    }
+    SDL_Texture* m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+    if (m_pTexture == 0) {
+        std::cout << "m_pTexture fail\n";
+        return false; // renderer init fail
+    }
+    SDL_FreeSurface(pTempSurface);
+//    SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+//    m_sourceRectangle.w = 50;
+//    m_sourceRectangle.h = 50;
+//
+//    m_destinationRectangle.x = m_sourceRectangle.x = 0;
+//    m_destinationRectangle.y = m_sourceRectangle.y = 0;
+//    m_destinationRectangle.w = m_sourceRectangle.w;
+//    m_destinationRectangle.h = m_sourceRectangle.h;
+    
     m_bRunning = true; // everything inited successfully, start the main loop
     return true;
 }
 
 void Game::render() {
     SDL_RenderClear(m_pRenderer); // clear the renderer to the draw color
+    
+//    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+    SDL_RenderCopy(m_pRenderer, m_pTexture, 0, 0);
+    
     SDL_RenderPresent(m_pRenderer); // draw to the screen
 }
 
