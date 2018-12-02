b6975d46f417977899ba6d0630ca46008d2ce258
anaida-gasparyan <anaida.gasparyan@gmail.com>
2018-12-02 17:31:56 +0100

    fix gitignore

---
+++ sdlgame1/include/TextureManager.h
@@ -0,0 +1,13 @@
+//
+//  TextureManager.h
+//  sdlgame1
+//
+//  Created by Anaida Gasparyan on 12/2/18.
+//  Copyright © 2018 Anaida Gasparyan. All rights reserved.
+//
+
+#ifndef TextureManager_h
+#define TextureManager_h
+
+
+#endif /* TextureManager_h */
---
+++ sdlgame1/src/TextureManager.cpp
@@ -0,0 +1,9 @@
+//
+//  TextureManager.cpp
+//  sdlgame1
+//
+//  Created by Anaida Gasparyan on 12/2/18.
+//  Copyright © 2018 Anaida Gasparyan. All rights reserved.
+//
+
+#include <stdio.h>
